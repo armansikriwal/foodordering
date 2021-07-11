239b3933762a689b498cc9668c876f37c4eaec49
armansikriwal <armankumarsikriwal3@gmail.com>
2021-07-12 00:49:16 +0530

    "commiting after release on azure"

--- Hotel_Arman/settings.py
+++ Hotel_Arman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p4@0#x$&*-!!io=&9jab+%m)zeq^h9nx8$3*8j#qun)$9c3oxz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foodme.azurewebsites.net','127.0.0.1','localhost']
 
 
 # Application definition
